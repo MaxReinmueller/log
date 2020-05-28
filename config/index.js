2ed99ceb0c82668631769be935c5f17e7d66c116
Max Reinmueller <maxreinmueller@gmail.com>
2020-05-28 13:48:54 -0700

    configure mongo atlas

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 const dbuser = process.env.DB_USER;
 const dbpassword = process.env.DB_PASS;
 
-const MONGODB_URI = `mongodb://${dbuser}:${dbpassword}@ds257752.mlab.com:57752/heroku_bzbd8smb`;
+const MONGODB_URI = `mongodb+srv://${dbuser}:${dbpassword}@log-dcuyc.mongodb.net/test?retryWrites=true&w=majority`;
 
 module.exports = MONGODB_URI;
