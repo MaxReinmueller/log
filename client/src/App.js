44d73e59cfa3ad5411ed50d50b60f559f941d027
Max Reinmueller <maxreinmueller@gmail.com>
2020-05-28 12:09:19 -0700

    added 404 page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Books from './pages/Books';
 import Detail from './pages/Detail';
-// import NoMatch from './pages/NoMatch';
+import NoMatch from './pages/NoMatch';
 import Nav from './components/Nav';
 
 const App = () => (
@@ -14,7 +14,7 @@ const App = () => (
 				<Route exact path="/" component={Books} />
 				<Route exact path="/books" component={Books} />
 				<Route exact path="/books/:id" component={Detail} />
-				{/* <Route component={NoMatch} /> */}
+				<Route component={NoMatch} />
 			</Switch>
 		</div>
 	</Router>
